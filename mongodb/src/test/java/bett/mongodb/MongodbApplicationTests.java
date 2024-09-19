6fc069b8c139cf3600a6806e5b3bf4b751e8311d
b10101 <b10101@gmail.com>
2024-09-19 14:28:14 +0300

    Mongodb atlas intergration

---
+++ mongodb/src/test/java/bett/mongodb/MongodbApplicationTests.java
@@ -0,0 +1,14 @@
+package bett.mongodb;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbbotMongoAtlasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
